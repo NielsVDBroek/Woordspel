e0414588af9c1c2f530b654f68c976c528921320
NielsVDBroek <Niels.BroekVanDen@gmail.com>
2024-06-28 19:39:54 +0200

    FIX Database connection

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -11,8 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('user_type')->default('user');
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
         });
     }
 
@@ -21,8 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('menu', function (Blueprint $table) {
-            $table->dropColumn(['user_type']);
-        });
+        Schema::dropIfExists('password_reset_tokens');
     }
 };
